a7eb12b56801d773631699a5c4fe0324ff7a8458
SKapturska <skapturska@us.edu.pl>
2016-10-30 15:21:39 +0100

    First function

--- src/textfiles/FileRead.java
+++ src/textfiles/FileRead.java
@@ -1,8 +1,33 @@
 package textfiles;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileRead {
     
-  
+  public int countLines(File file){
+        int lines = 0;
+        int letter;
+        int previousLetter = 0;
+        try { 
+            FileReader fr = new FileReader(file);
+            while((letter = fr.read()) != -1){
+                if(previousLetter == 13 && letter == 10){
+                    lines++;
+                }
+                previousLetter = letter;
+            }
+            
+            fr.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return lines;
+    }
+
+
+
     
    
 
--- src/textfiles/TextFiles.java
+++ src/textfiles/TextFiles.java
@@ -1,11 +1,15 @@
 
 package textfiles;
 
+import java.io.File;
 
 public class TextFiles {
 
     
     public static void main(String[] args) {
+          
+       FileRead fr = new FileRead();
+       System.out.println(fr.countLines(new File("plik.txt")));
      
         
         
