2e7952a23db8fdcf0d338c9539a52b43adf7705d
SKapturska <skapturska@us.edu.pl>
2016-10-31 10:37:11 +0100

    Koniec

--- src/textfiles/FileRead.java
+++ src/textfiles/FileRead.java
@@ -1,35 +1,55 @@
 package textfiles;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FileRead {
-    
-  public int countLines(File file){
-        int lines = 0;
+
+    public int countLines(File file) {
+        int lines = 1;
         int letter;
         int previousLetter = 0;
-        try { 
+        try {
             FileReader fr = new FileReader(file);
-            while((letter = fr.read()) != -1){
-                if(previousLetter == 13 && letter == 10){
+            while ((letter = fr.read()) != -1) {
+                if (previousLetter == 13 && letter == 10) {
                     lines++;
                 }
                 previousLetter = letter;
             }
-            
+
             fr.close();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
         return lines;
     }
-
-
-
     
-   
+    public int countWords(File file){
+        int word = 0;
+        try {
+            BufferedReader f = new BufferedReader(new FileReader(file));
+            String line = f.readLine();
+            while(line != null){
+                word++;
+                for(int i = 0; i < line.length() - 1; i++){
+                    if((line.charAt(i) == ' ' || line.charAt(i) == '\t') || line.charAt(i) == '\n'){
+                        word++;
+                    }
+                }
+                line = f.readLine();  
+            }
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+      return word;  
+    }
 
-    
 }
--- src/textfiles/TextFiles.java
+++ src/textfiles/TextFiles.java
@@ -1,18 +1,14 @@
-
 package textfiles;
 
 import java.io.File;
 
 public class TextFiles {
 
-    
     public static void main(String[] args) {
-          
-       FileRead fr = new FileRead();
-       System.out.println(fr.countLines(new File("plik.txt")));
-     
-        
-        
+
+        FileRead fr = new FileRead();
+        System.out.println("Liczba wierszy: " + fr.countLines(new File("plik.txt")));
+        System.out.println("Liczba słów: " + fr.countWords(new File("plik.txt")));
     }
-    
+
 }
